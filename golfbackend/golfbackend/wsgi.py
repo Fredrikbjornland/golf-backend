54f5725c644e15130753acc4d3104cf92efcd63c
Fredrik Bjørnland <bjornlandfredrik@gmail.com>
2025-03-30 13:50:05 +0200

    new config

--- golfbackend/golfbackend/wsgi.py
+++ golfbackend/golfbackend/wsgi.py
@@ -8,9 +8,29 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import time
+import traceback
+import signal
+import sys
 
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "golfbackend.settings")
 
-application = get_wsgi_application()
+if getattr(settings, "PROJECT_ROOT_DIR", None):
+    sys.path.append(settings.PROJECT_ROOT_DIR)
+
+if getattr(settings, "LIBRARY_ROOT_DIR", None):
+    sys.path.append(settings.LIBRARY_ROOT_DIR)
+
+try:
+    application = get_wsgi_application()
+except Exception:
+    # Error loading applications
+    if "mod_wsgi" in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
+
